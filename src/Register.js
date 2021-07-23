ce05bbfcd706d6682958ddf6787d7e4255a254e2
MarcusLopes <marcuslopesdf@yahoo.com.br>
2021-07-23 11:09:37 -0300

    Implementação de Logout e condition rendering dos cabeçalhos

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom'
+import Header  from './Header';
 
 function Register()
 {
+    useEffect(()=> {
+        if(localStorage.getItem('user-info'))
+        {
+            history.push("/add");
+        }
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ ]);
+
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail  ] = useState("");
@@ -29,6 +39,8 @@ function Register()
     }
     
     return (
+        <>
+        <Header/>
         <div className="col-sm-6 offset-sm-3">
             <h1>Register Page</h1>
             <input type="text" value={name} onChange={ (e)=>setName(e.target.value)} className="form-control" placeholder="name" />
@@ -38,8 +50,8 @@ function Register()
             <input type="text"  value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="e-mail" />
             <br />
             <button onClick={signUp} className="btn btn-primary">Sign Up</button>
-            
         </div>
+        </>
     )
 }
 
